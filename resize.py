4010c7f5b5d491a6dd8af1f18b2b3539517f8874
Valen-Nneji-007 <162584002+Valen-Nneji-007@users.noreply.github.com>
2025-09-16 00:09:10 +0200

    different resize method

---
+++ resize.py
@@ -0,0 +1,35 @@
+import cv2 as cv
+import matplotlib.pyplot as plt
+
+def ImageResize():
+    Resize = cv.imread("C:/Users/Administrator/Documents/vs_code/computer_vision/opencv_intro/opencv_intro/opencv_intro/me.jpg")
+    img = cv.resize(Resize,(342,512))
+    img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+    img = img[286:300,311:400,:]
+    height,width,_ = img.shape
+
+    scale = 1/4
+
+    interMethods = [
+        cv.INTER_AREA,
+        cv.INTER_LINEAR,
+        cv.INTER_NEAREST,
+        cv.INTER_CUBIC,
+        cv.INTER_LANCZOS4
+    ]
+
+    interpTitle = ['area','linear','nearest','cubic','lanczos4']
+
+    plt.figure()
+    plt.subplot(2,3,1)
+    plt.imshow(img)
+
+    for i in range(len(interMethods)):
+        plt.subplot(2,3,i+2)
+        imgResize = cv.resize(img,(int(width*scale),int(height*scale)),
+        interploation= interMethods)
+        plt.imshow(imgResize)
+        plt.title(interpTitle[i])
+
+if __name__=='__main__':
+    ImageResize()
